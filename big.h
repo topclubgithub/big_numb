df263797133e18f6b0d2719de1c68a8dcd27a2df
topclubgithub <1690394428@qq.com>
2017-04-02 22:59:49 +0800

    Add files via upload

---
+++ big.h
@@ -0,0 +1,4 @@
+char * bigadd(char *adda,int lena,char *addb,int lenb);
+char * bigsub(char *suba,int lena,char *subb,int lenb);
+char * bigmul(char *m,int lena,char *f,int lenb);
+char * bigdiv(char *diva,int lena,char *divb,int lenb);
